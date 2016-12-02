ea39a956c1712c904402b9c0dcf645aae52d2eb6
David Alves <dalves@fb.com>
2016-12-01 17:17:22 -0800

    removed unused function

--- verify/ps13_5.py
+++ verify/ps13_5.py
@@ -1,22 +1,6 @@
 #!/usr/bin/env python3
 import random
 
-def choose(n, k, memo={(0,0): 1}):
-    t = (n,k)
-    if t in memo:
-        return memo[t]
-    if n == 0 or k > n:
-        return 0
-    if k == 0:
-        return 1
-    if k > n//2:
-        return choose(n, n-k)
-    val = choose(n-1, k-1) + choose(n-1, k)
-    memo[t] = val
-    return val
-
-
-
 def is_close(n):
     v = random.getrandbits(2*n)
     return bin(v).count('1') == n
